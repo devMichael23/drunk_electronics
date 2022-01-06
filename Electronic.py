f00d601eedc02fa68885c9f37b8333ef2fa667b0
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2022-01-06 12:59:12 +0300

    [/] Remove function from Electronic.py

--- Electronic.py
+++ Electronic.py
@@ -3,22 +3,9 @@ class Electronic:
         self.size = 1
         self.rad = 0
         self.vision = 0
+        self.fuel = type("", (), dict(need=True, bank=25))()
         self.fuel = {
             "need": True,
             "bank": 25
         }
-        self.location = {
-            "x": 0,
-            "y": 0
-        }
-
-    def move(self, x, y):
-        if self.fuel['need']:
-            if self.fuel['bank'] > 0:
-                self.fuel["bank"] = self.fuel["bank"] - 1
-            else:
-                exit(0)
-        self.location = {
-            'x': x,
-            'y': y
-        }
+        self.location = type("", (), dict(x=0, y=0))()

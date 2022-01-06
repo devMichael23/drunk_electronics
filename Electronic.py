78080c7aa7bf3383d828f82e31714b8dc046bd1b
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2022-01-06 22:26:11 +0300

    [/] Add Electronic.py api

--- Electronic.py
+++ Electronic.py
@@ -5,3 +5,27 @@ class Electronic:
         self.vision = 0
         self.fuel = type("", (), dict(need=True, bank=25))()
         self.location = type("", (), dict(x=0, y=0))()
+
+    def get_fuel_need(self):
+        return self.fuel.need
+
+    def get_fuel_bank(self):
+        return self.fuel.bank
+
+    def set_fuel_need(self, param):
+        self.fuel.need = param
+
+    def set_fuel_bank(self, param):
+        self.fuel.bank = param
+
+    def get_location_x(self):
+        return self.location.x
+
+    def get_location_y(self):
+        return self.location.y
+
+    def set_location_x(self, param):
+        self.location.x = param
+
+    def set_location_y(self, param):
+        self.location.y = param

9a6c067bdd202907ece305fea04825d45a723432
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2022-01-06 13:04:04 +0300

    [/] Rewrite dict get

--- Memory.py
+++ Memory.py
@@ -6,23 +6,21 @@ class Memory:
     def __init__(self, w, h):
         self.silicon_world = SiliconWorld(w, h)
         self.electronic = Electronic()
-        self.silicon_world.set_params_to_map(self.electronic.location['x'], self.electronic.location['y'], 5)
+        self.silicon_world.set_params_to_map(self.electronic.location.x, self.electronic.location.y, 5)
 
     def move_electronic(self, x, y):
-        if self.electronic.fuel['need']:
-            if self.electronic.fuel['bank'] > 0:
-                self.electronic.fuel["bank"] = self.electronic.fuel["bank"] - 1
+        if self.electronic.fuel.need:
+            if self.electronic.fuel.bank > 0:
+                self.electronic.fuel.bank = self.electronic.fuel.bank - 1
             else:
                 exit(0)
-        self.silicon_world.set_params_to_map(self.electronic.location['x'], self.electronic.location['y'], 1)
-        self.electronic.location = {
-            'x': x,
-            'y': y
-        }
+        self.silicon_world.set_params_to_map(self.electronic.location.x, self.electronic.location.y, 1)
+        self.electronic.location.x = x
+        self.electronic.location.y = y
         self.update_electronic_pos()
 
     def update_electronic_pos(self):
-        self.silicon_world.set_params_to_map(self.electronic.location['x'], self.electronic.location['y'], 5)
+        self.silicon_world.set_params_to_map(self.electronic.location.x, self.electronic.location.y, 5)
 
     def get_silicon_world(self):
         return self.silicon_world

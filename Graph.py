169b863bb305ef86177e63892d89498c67107010
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2022-01-06 13:24:21 +0300

    [/] Rewrite str get Node & dict id get in Graph.py

--- Graph.py
+++ Graph.py
@@ -1,4 +1,5 @@
-from Memory import Memory
+from Memory import *
+
 
 class Steps:
     def __init__(self, up=None, down=None, left=None, right=None):
@@ -8,17 +9,20 @@ class Steps:
         self.right = right
 
     def get_steps(self):
-        return '{\n\t\'up\':\t ' + str(self.up) + ',\n\t\'down\':\t ' + str(self.down) + ',\n\t\'left\':\t  ' \
-               + str(self.left) + ',\n\t\'right\': ' + str(self.right) + "\n}"
+        return '{\n\t\t\'up\':\t  ' + str(self.up) + ',\n\t\t\'down\':\t  ' + str(self.down) + ',\n\t\t\'left\':\t  ' \
+               + str(self.left) + ',\n\t\t\'right\':  ' + str(self.right) + "\n\t}"
 
 
 class Node:
-    def __init__(self, steps=None, is_electronic=False):
+    def __init__(self, id=None, steps=None, is_electronic=False):
+        self.id = type("", (), dict(x=id[0], y=id[1]))()
         self.steps = Steps(steps[0], steps[1], steps[2], steps[3])
         self.isElectronic = is_electronic
 
     def __str__(self):
-        s = self.steps.get_steps() + "\n" + str(self.isElectronic)
+        id = '{\n\tid: {' + str(self.id.x) + '; ' + str(self.id.y) + '},\n\t'
+        boolean = 'isElectronic: ' + str(self.isElectronic) + '\n\t'
+        s = id + boolean + self.steps.get_steps() + '\n}'
         return s
 
 

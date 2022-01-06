accdba42df4f9ea2e60123e9c861e693ac0d5530
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2022-01-06 13:44:56 +0300

    [/] Refactor SiliconWorld.py

--- SiliconWorld.py
+++ SiliconWorld.py
@@ -4,7 +4,9 @@ from random import randint
 
 class SiliconWorld:
     def __init__(self, w=10, h=10):
-        self.SiliconWorld = [[1 for _ in range(0, w)] for _ in range(0, h)]
+        self.w = w
+        self.h = h
+        self.SiliconWorld = [[1 for _ in range(0, self.w)] for _ in range(0, self.h)]
         self.generate_map()
 
     def create_fuel(self):
@@ -15,32 +17,32 @@ class SiliconWorld:
                     self.SiliconWorld[i][j] = 2
 
     def generate_map(self):
-        for i in range(0, len(self.SiliconWorld) - 1):
+        for i in range(0, self.h - 1):
             j = 0
-            rand_step = randint(0, len(self.SiliconWorld[i]) // 2)
+            rand_step = randint(0, self.w // 2)
             j = j + rand_step
             self.SiliconWorld[i][j] = 0
             self.SiliconWorld[i][j + 1] = 0
             self.SiliconWorld[i + 1][j] = 0
             self.SiliconWorld[i + 1][j + 1] = 0
-            rand_step = randint(0, len(self.SiliconWorld[i]) // 2)
-            while j + rand_step > len(self.SiliconWorld[i]) - 2:
-                rand_step = randint(0, len(self.SiliconWorld[i]) // 2)
+            rand_step = randint(0, self.w // 2)
+            while j + rand_step > self.w - 2:
+                rand_step = randint(0, self.w // 2)
             j = j + rand_step
             self.SiliconWorld[i][j] = 0
             self.SiliconWorld[i][j + 1] = 0
             self.SiliconWorld[i + 1][j] = 0
             self.SiliconWorld[i + 1][j + 1] = 0
-            if i < len(self.SiliconWorld) - 4:
-                i + len(self.SiliconWorld) // 4
+            if i < self.h - 4:
+                i + self.h // 4
 
         self.create_fuel()
 
-        end_x = randint(0, len(self.SiliconWorld[0]))
-        end_y = randint(0, len(self.SiliconWorld))
+        end_x = randint(0, self.w)
+        end_y = randint(0, self.h)
         while self.SiliconWorld[end_x][end_y] == 0:
-            end_x = randint(0, len(self.SiliconWorld[0]))
-            end_y = randint(0, len(self.SiliconWorld))
+            end_x = randint(0, self.w)
+            end_y = randint(0, self.h)
 
         self.SiliconWorld[end_x][end_y] = 3
 
@@ -48,8 +50,8 @@ class SiliconWorld:
         self.SiliconWorld[x][y] = param
 
     def print_map(self):
-        for i in range(0, len(self.SiliconWorld)):
-            for j in range(0, len(self.SiliconWorld[i])):
+        for i in range(0, self.h):
+            for j in range(0, self.w):
                 if self.SiliconWorld[i][j] == 5:
                     print(colored("|♌|", 'magenta', attrs=['bold']), end='')
                 elif self.SiliconWorld[i][j] == 3:
@@ -61,3 +63,12 @@ class SiliconWorld:
                 elif self.SiliconWorld[i][j] == 0:
                     print(colored("|#|", 'green'), end='')
             print("\n", end="")
+
+    def get_weight(self):
+        return self.w
+
+    def get_hight(self):
+        return self.h
+
+    def get_map_atom(self, h, w):
+        return self.SiliconWorld[h][w]

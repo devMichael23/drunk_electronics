eff8921aa36531b46a15c32419286f1d802fc184
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2022-01-06 22:31:07 +0300

    [/] Work with safe data

--- Memory.py
+++ Memory.py
@@ -4,26 +4,26 @@ from Electronic import Electronic
 
 class Memory:
     def __init__(self, w, h):
-        self.silicon_world = SiliconWorld(w, h)
-        self.electronic = Electronic()
-        self.silicon_world.set_params_to_map(self.electronic.location.x, self.electronic.location.y, 5)
+        self.__silicon_world = SiliconWorld(w, h)
+        self.__electronic = Electronic()
+        self.__silicon_world.set_params_to_map(self.__electronic.location.x, self.__electronic.location.y, 5)
 
-    def move_electronic(self, y, x):
-        if self.electronic.fuel.need:
-            if self.electronic.fuel.bank > 0:
-                self.electronic.fuel.bank = self.electronic.fuel.bank - 1
+    def move_electronic(self, x, y):
+        if self.__electronic.fuel.need:
+            if self.__electronic.fuel.bank > 0:
+                self.__electronic.fuel.bank = self.__electronic.fuel.bank - 1
             else:
                 exit(0)
-        self.silicon_world.set_params_to_map(self.electronic.location.x, self.electronic.location.y, 1)
-        self.electronic.location.x = x
-        self.electronic.location.y = y
+        self.__silicon_world.set_params_to_map(self.__electronic.location.x, self.__electronic.location.y, 1)
+        self.__electronic.location.x = x
+        self.__electronic.location.y = y
         self.update_electronic_pos()
 
     def update_electronic_pos(self):
-        self.silicon_world.set_params_to_map(self.electronic.location.x, self.electronic.location.y, 5)
+        self.__silicon_world.set_params_to_map(self.__electronic.location.x, self.__electronic.location.y, 5)
 
     def get_silicon_world(self):
-        return self.silicon_world
+        return self.__silicon_world
 
     def get_electronic(self):
-        return self.electronic
+        return self.__electronic
--- SiliconWorld.py
+++ SiliconWorld.py
@@ -4,50 +4,50 @@ from random import randint
 
 class SiliconWorld:
     def __init__(self, w=10, h=10):
-        self.w = w
-        self.h = h
-        self.SiliconWorld = [[1 for _ in range(0, self.w)] for _ in range(0, self.h)]
+        self.__w = w
+        self.__h = h
+        self.__SiliconWorld = [[1 for _ in range(0, self.__w)] for _ in range(0, self.__h)]
         self.generate_map()
 
     def create_fuel(self):
-        for i in range(0, len(self.SiliconWorld)):
-            for j in range(0, len(self.SiliconWorld[i])):
+        for i in range(0, self.get_hight()):
+            for j in range(0, self.get_weight()):
                 fuel = randint(0, 19)
                 if fuel == 1:
-                    self.SiliconWorld[i][j] = 2
+                    self.set_params_to_map(i, j, 2)
 
     def generate_map(self):
-        for i in range(0, self.h - 1):
+        for i in range(0, self.__h - 1):
             j = 0
-            rand_step = randint(0, self.w // 2)
+            rand_step = randint(0, self.__w // 2)
             j = j + rand_step
-            self.SiliconWorld[i][j] = 0
-            self.SiliconWorld[i][j + 1] = 0
-            self.SiliconWorld[i + 1][j] = 0
-            self.SiliconWorld[i + 1][j + 1] = 0
-            rand_step = randint(0, self.w // 2)
-            while j + rand_step > self.w - 2:
-                rand_step = randint(0, self.w // 2)
+            self.__SiliconWorld[i][j] = 0
+            self.__SiliconWorld[i][j + 1] = 0
+            self.__SiliconWorld[i + 1][j] = 0
+            self.__SiliconWorld[i + 1][j + 1] = 0
+            rand_step = randint(0, self.__w // 2)
+            while j + rand_step > self.__w - 2:
+                rand_step = randint(0, self.__w // 2)
             j = j + rand_step
-            self.SiliconWorld[i][j] = 0
-            self.SiliconWorld[i][j + 1] = 0
-            self.SiliconWorld[i + 1][j] = 0
-            self.SiliconWorld[i + 1][j + 1] = 0
-            if i < self.h - 4:
-                i + self.h // 4
+            self.__SiliconWorld[i][j] = 0
+            self.__SiliconWorld[i][j + 1] = 0
+            self.__SiliconWorld[i + 1][j] = 0
+            self.__SiliconWorld[i + 1][j + 1] = 0
+            if i < self.__h - 4:
+                i + self.__h // 4
 
         self.create_fuel()
 
-        end_x = randint(0, self.w)
-        end_y = randint(0, self.h)
-        while self.SiliconWorld[end_x][end_y] == 0:
-            end_x = randint(0, self.w)
-            end_y = randint(0, self.h)
+        end_x = randint(0, self.__w)
+        end_y = randint(0, self.__h)
+        while self.__SiliconWorld[end_x][end_y] == 0:
+            end_x = randint(0, self.__w)
+            end_y = randint(0, self.__h)
 
-        self.SiliconWorld[end_x][end_y] = 3
+        self.set_params_to_map(end_x, end_y, 3)
 
     def set_params_to_map(self, x, y, param):
-        self.SiliconWorld[y][x] = param
+        self.__SiliconWorld[y][x] = param
 
     def __str__(self):
         result = ''
@@ -67,10 +67,10 @@ class SiliconWorld:
         return result
 
     def get_weight(self):
-        return self.w
+        return self.__w
 
     def get_hight(self):
-        return self.h
+        return self.__h
 
     def get_map_atom(self, h, w):
-        return self.SiliconWorld[h][w]
+        return self.__SiliconWorld[h][w]

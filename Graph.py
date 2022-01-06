8952472fbd51c366a4b35a3f463f6653222d4e3b
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2022-01-06 12:30:41 +0300

    [/] Create new class Node into Graph.py

--- Graph.py
+++ Graph.py
@@ -8,3 +8,12 @@ class Steps:
     def get_steps(self):
         return '{\n\t\'up\':\t ' + str(self.up) + ',\n\t\'down\':\t ' + str(self.down) + ',\n\t\'left\':\t  ' \
                + str(self.left) + ',\n\t\'right\': ' + str(self.right) + "\n}"
+
+class Node:
+    def __init__(self, steps=None, is_electronic=False):
+        self.steps = Steps(steps[0], steps[1], steps[2], steps[3])
+        self.isElectronic = is_electronic
+
+    def __str__(self):
+        s = self.steps.get_steps() + "\n" + str(self.isElectronic)
+        return s

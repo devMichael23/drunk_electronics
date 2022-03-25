f256c09f212af8e6e739d099af4f6f27d5072342
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2022-03-25 12:34:36 +0300

    refactor gitignore[3]

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ from Api.getters import *
 mem = Memory(25, 25)
 g = Graph(mem)
 copy = mem
+fdgsdfg
+
+fgsdf
+
 print(mem.get_silicon_world())
 
-
+dfgsdg
+dfsg
+sdfg
+sdfg
 path = get_path(g, mem)
 
 
 print(mem.get_silicon_world())
+print(path)
